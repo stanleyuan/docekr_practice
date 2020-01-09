7df0808c1226c87d129829391e11c91d9ca8c709
stanleyuan <stanley830912@gmail.com>
2020-01-09 09:59:40 +0800

    Add nginx command

---
+++ nginx/nginx.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+#===============================================================================
+#
+#          FILE:  nginx.sh
+#
+#         USAGE:  ./nginx.sh
+#
+#   DESCRIPTION:  Run in nginx conainer
+#
+#       OPTIONS:  ---
+#  REQUIREMENTS:  ---
+#          BUGS:  ---
+#         NOTES:  ---
+#        AUTHOR:  Stanley Yuan (), None
+#       COMPANY:  None
+#       VERSION:  1.0
+#       CREATED:  06/10/2019 04:41:18 PM CST
+#      REVISION:  ---
+#===============================================================================
+
+echo "$1"
